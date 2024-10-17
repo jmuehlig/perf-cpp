33525794437c63e9d8b7c2c2d273104440bfd14d
Jan Mühlig <jan.muehlig@tu-dortmund.de>
2024-10-17 18:46:34 +0200

    Fixed bug in MultiSamplerBase: Sort by time if only one sampler and no timestamp provided.

--- src/sampler.cpp
+++ src/sampler.cpp
@@ -749,7 +749,7 @@ perf::MultiSamplerBase::result(const std::vector<Sampler>& sampler, bool sort_by
     auto result = sampler.front().result();
 
     sort_by_time &= sampler.front()._values.is_set(PERF_SAMPLE_TIME);
-    
+
     for (auto i = 1U; i < sampler.size(); ++i) {
       /// Only sort if all samplers recorded the timestamp.
       sort_by_time &= sampler[i]._values.is_set(PERF_SAMPLE_TIME);
