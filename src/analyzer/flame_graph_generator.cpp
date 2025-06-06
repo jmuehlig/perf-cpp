649cf2dc96188d4bd6b0692c8f0edcb930eeef57
Jan Mühlig <jan.muehlig@tu-dortmund.de>
2025-06-06 12:12:04 +0100

    Added missing <algorithm> header to flame graph generator.

--- src/analyzer/flame_graph_generator.cpp
+++ src/analyzer/flame_graph_generator.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <iterator>
+#include <algorithm>
 #include <perfcpp/analyzer/flame_graph_generator.h>
 
 std::optional<std::reference_wrapper<const perf::SymbolResolver::Symbol>>
